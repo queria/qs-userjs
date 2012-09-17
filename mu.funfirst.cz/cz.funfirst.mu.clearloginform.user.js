1c60dc65a0ca6daeec2770679124b8ad72888df7
Queria Sa-Tas <queria@sa-tas.net>
2012-09-17 11:45:47 +0200

    FFMU ClearLoginForm - updated meta info

--- mu.funfirst.cz/cz.funfirst.mu.clearloginform.user.js
+++ mu.funfirst.cz/cz.funfirst.mu.clearloginform.user.js
@@ -1,14 +1,15 @@
 // ==UserScript==
-// // @id             cz.funfirst.mu.clearloginform
-// // @name           FunFirstMU Clear Login Form
-// // @version        1.0
-// // @namespace      
-// // @author         Queria Sa-Tas <public@sa-tas.net>
-// // @description    Clears default values (description) from login form to enable credentials autocomplete in browser.
-// // @include        http://muonline.funfirst.cz/
-// // @include        http://muonline.funfirst.cz/*.php
-// // @run-at         document-end
-// // ==/UserScript==
+// @author         Queria Sa-Tas <public@sa-tas.net>
+// @namespace      http://github.com/queria/qs-userjs/
+// @id             cz.funfirst.muonline.clearloginform
+// @name           FunFirstMU Clear Login Form
+// @version        1.1
+// @updateURL      https://raw.github.com/queria/qs-userjs/master/mu.funfirst.cz/cz.funfirst.mu.clearloginform.user.js
+// @include        http://muonline.funfirst.cz/
+// @include        http://muonline.funfirst.cz/*.php
+// @description    Clears default values (description) from login form to enable credentials autocomplete in browser.
+// @run-at         document-end
+// ==/UserScript==
 var lForm = document.forms.namedItem("uss_login_form");
 lForm.elements.namedItem('uss_password').value = '';
 lForm.elements.namedItem('uss_id').value = '';
